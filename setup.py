f477041c304fef7512e0da8390877975b3ba05ce
Joe Hillenbrand <joehillen@gmail.com>
2014-05-13 12:20:26 -0700

    Add author email

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     name='jsonrpc-ns',
     version='0.5',
     description='Lightweight JSON-RPC 2.0 library for building TCP (Netstring) clients',
-    author='Flowroute',
+    author='Flowroute LLC',
+    author_email='joe@flowroute.com',
     url='http://github.com/flowroute/jsonrpc-ns',
     packages=['jsonrpc_ns']
 )
