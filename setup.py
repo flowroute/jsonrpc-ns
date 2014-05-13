3705c2e37548a0ac1984f6158e777780d7cea8b4
Joe Hillenbrand <joehillen@gmail.com>
2014-05-13 12:15:27 -0700

    version 0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 setup(
     name='jsonrpc-ns',
-    version='0.4',
+    version='0.5',
     description='Lightweight JSON-RPC 2.0 library for building TCP (Netstring) clients',
     author='Flowroute',
     url='http://github.com/flowroute/jsonrpc-ns',
